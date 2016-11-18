341aa837237e0a774a3dd8e86a70c94ca9b21bbf
Cenaras <mathias_rud@hotmail.com>
2016-11-18 09:39:58 +0100

    Added a lot of stuff

---
+++ P5/app/Snake.js
@@ -0,0 +1,27 @@
+//The coordinates of the Snake
+var x;
+var y;
+
+function Snake() {
+  this.x = x;
+  this.y = y;
+  this.xSpeed = 0;
+  this.ySpeed = 0;
+  this.size = [];
+  
+  //Sets the direction of the Snake
+  this.dir = function(x, y) {
+    s.x = xSpeed;
+    s.y = ySpeed;
+  }
+  
+  //Displays the Snake
+  this.display = function() {
+    rect();
+  }
+  //Updates the location and size of the Snake
+  this.update = function() {
+    
+  }
+  
+}
--- P5/app/sketch.js
+++ P5/app/sketch.js
@@ -1,22 +1,36 @@
-
+var s;
+var scl = 20;
 
 function setup() {
   createCanvas(600, 600);
-  
-  
-  
+
+  s = new Snake();
+
+
+
 }
 
 function draw() {
   background(70);
-  
+
 }
 
-function snake() {
-  this.x = x;
-  this.y = y;
-  this.xSpeed = 0;
-  this.ySpeed = 0;
-  this.size = [];
-  
+function pickLocation() {
+  var cols = floor((width / scl));
+  var rows = floor((height / scl));
+}
+
+function keyPressed() {
+  if (keyCode == UP_ARROW) {
+    dir(0, -1);
+  }
+  if (keyCode == DOWN_ARROW) {
+    dir(0, 1);
+  }
+  if (keyCode == LEFT_ARROW) {
+    dir(-1, 0);
+  }
+  if (keyCode == RIGHT_ARROW) {
+    dir(1, 0);
+  }
 }
