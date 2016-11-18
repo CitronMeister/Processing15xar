5da8dbec3d9e1eee9d083dd665107dc81edc01b0
Cenaras <mathias_rud@hotmail.com>
2016-11-18 09:15:14 +0100

    GÅ VÆK MENNESKER

--- P5/app/sketch.js
+++ P5/app/sketch.js
@@ -1,8 +1,22 @@
+
+
 function setup() {
-  createCanvas();
+  createCanvas(600, 600);
+  
+  
+  
 }
 
 function draw() {
+  background(70);
+  
+}
 
+function snake() {
+  this.x = x;
+  this.y = y;
+  this.xSpeed = 0;
+  this.ySpeed = 0;
+  this.size = size[];
   
 }
